b0c86f51a8f1f81cd0c234a02d582be9e2419a50
YBogdan RADOI <radoi.office@gmail.com>
2022-04-09 18:56:22 +0200

    Create fake data for database

--- Data/DatabaseContext.cs
+++ Data/DatabaseContext.cs
@@ -13,11 +13,203 @@ namespace Colomb.Data
         // Define Constructor
         public DatabaseContext(DbContextOptions options) : base(options)
         {}
+        public DbSet<Review> Reviews { get; set; } // Reviews = the name that the db will use
+        public DbSet<Evenement> Evenements { get; set; }
+        public DbSet<Compte> Comptes { get; set; }
+        /*public DbSet<CompteLikerEvenement> CompteLikerEvenements { get; set; }*/
+
         protected override void OnModelCreating(ModelBuilder builder)
         {
+            // Fake data
+            builder.Entity<Compte>().HasData(
+                new Compte
+                {
+                    CompteId = 1,
+                    Email = "bogdan@gmail.com",
+                    Login = "bogdan",
+                    Password = "bogdan",
+                    Nom = "Radoi",
+                    Prenom = "Bogdan",
+                    Adresse = "40 rue de Compte",
+                    DOB = new DateTime(10 / 12 / 1988),
+                    Photo = "chemin/photo_compte_profil",
+                    Description = "Description",
+                    Role = "ROLE_USER",
+                    VisibiliteReviews = true,
+                    estValide = true,
+                    NumeroSiret = null
+                },
+                new Compte
+                {
+                    CompteId = 2,
+                    Email = "wally@gmail.com",
+                    Login = "wally",
+                    Password = "wally",
+                    Nom = "Cisse",
+                    Prenom = "Wally",
+                    Adresse = "40 rue de Paris",
+                    DOB = new DateTime(10 / 12 / 1988),
+                    Photo = "dossier/photo",
+                    Description = "Description",
+                    Role = "ROLE_USER",
+                    VisibiliteReviews = true,
+                    estValide = true,
+                    NumeroSiret = null
+                },
+                new Compte
+                {
+                    CompteId = 3,
+                    Email = "daria@gmail.com",
+                    Login = "daria",
+                    Password = "Daria",
+                    Nom = "Rychkova",
+                    Prenom = "daria",
+                    Adresse = "40 rue de Paris",
+                    DOB = new DateTime(10 / 12 / 1988),
+                    Photo = "dossier/photo",
+                    Description = "Description",
+                    Role = "ROLE_USER",
+                    VisibiliteReviews = true,
+                    estValide = true,
+                    NumeroSiret = null
+                }
+            );
+            builder.Entity<Evenement>().HasData(
+              new Evenement
+              {
+                  EvenementId = 1,
+                  Nom = "Evenement Comedie",
+                  Date = new DateTime(10 / 12 / 1988),
+                  HeureOuverture = new DateTime(2022, 12, 31, 9, 0, 0),
+                  HeureFermeture = new DateTime(2022, 12, 31, 18, 0, 0),
+                  Prix = 40,
+                  Adresse = "40 rue de Evenement",
+                  Categorie = "musee",
+                  Photo = "chemin/photo_evenement",
+                  Description = "Description evenement",
+                  NombrePersMax = 50,
+                  NombreLikes = 2,
+                  NombreVues = 40,
+                  EstSignale = false,
+                  EstSuspendu = false,
+                  Longitude = 40.22,
+                  Latitude = 22.22,
+                  CompteId = 1
+              },
+              new Evenement
+              {
+                  EvenementId = 2,
+                  Nom = "Evenement dehors",
+                  Date = new DateTime(10 / 06 / 2022),
+                  HeureOuverture = new DateTime(2022, 12, 31, 9, 0, 0),
+                  HeureFermeture = new DateTime(2022, 12, 31, 19, 0, 0),
+                  Prix = 300,
+                  Adresse = "40 rue de Evenement",
+                  Categorie = "theatre",
+                  Photo = "chemin/photo_evenement",
+                  Description = "Description evenement",
+                  NombrePersMax = 20,
+                  NombreLikes = 440,
+                  NombreVues = 140,
+                  EstSignale = false,
+                  EstSuspendu = false,
+                  Longitude = 55.22,
+                  Latitude = 11.22,
+                  CompteId = 2
+              },
+              new Evenement
+              {
+                  EvenementId = 3,
+                  Nom = "Evenement Parc",
+                  Date = new DateTime(01 / 02 / 2023),
+                  HeureOuverture = new DateTime(2022, 12, 31, 10, 0, 0),
+                  HeureFermeture = new DateTime(2022, 12, 31, 17, 0, 0),
+                  Prix = 146,
+                  Adresse = "40 rue de Evenement",
+                  Categorie = "spectacle",
+                  Photo = "chemin/photo_evenement",
+                  Description = "Description evenement",
+                  NombrePersMax = 250,
+                  NombreLikes = 112,
+                  NombreVues = 140,
+                  EstSignale = false,
+                  EstSuspendu = false,
+                  Longitude = 140.22,
+                  Latitude = 122.22,
+                  CompteId = 1
+              }
+            );
+            builder.Entity<Review>().HasData(
+              new Review
+              {
+                  ReviewId = 1,
+                  Date = new DateTime(2022, 12, 31, 17, 0, 0),
+                  NombreEtoiles = 2,
+                  NombreLikes = 2,
+                  Contenu = "Sympa mais pas plus, je veux veux un remboursement",
+                  EstSignale = true,
+                  CompteId = 2,
+                  EvenementId = 3,
+              },
+              new Review
+              {
+                  ReviewId = 2,
+                  Date = new DateTime(2022, 3, 14, 17, 0, 0),
+                  NombreEtoiles = 5,
+                  NombreLikes = 1,
+                  Contenu = "Tres bien",
+                  EstSignale = false,
+                  CompteId = 3,
+                  EvenementId = 2,
+              },
+              new Review
+              {
+                  ReviewId = 3,
+                  Date = new DateTime(2022, 2, 15, 17, 0, 0),
+                  NombreEtoiles = 1,
+                  NombreLikes = 0,
+                  Contenu = "Bref, je reviendrai pas",
+                  EstSignale = false,
+                  CompteId = 1,
+                  EvenementId = 1,
+              },
+              new Review
+              {
+                  ReviewId = 4,
+                  Date = new DateTime(2022, 5, 17, 17, 0, 0),
+                  NombreEtoiles = 3,
+                  NombreLikes = 11,
+                  Contenu = "Ingenieux et spectaculaire",
+                  EstSignale = false,
+                  CompteId = 2,
+                  EvenementId = 3,
+              },
+              new Review
+              {
+                  ReviewId = 5,
+                  Date = new DateTime(2022, 9, 19, 17, 0, 0),
+                  NombreEtoiles = 2,
+                  NombreLikes = 22,
+                  Contenu = "Moyen mais pas cher, je ne recommande pas",
+                  EstSignale = false,
+                  CompteId = 1,
+                  EvenementId = 3,
+              }
+           );
+
             builder.Entity<Compte>()
                 .HasMany(c => c.EvenementsLiked)
-                .WithMany(e => e.ComptesEvenementsLiked);
+                .WithMany(e => e.ComptesEvenementsLiked)
+                .UsingEntity(j => j.HasData(
+                    new { ComptesEvenementsLikedCompteId = 1, EvenementsLikedEvenementId = 3 },
+                    new { ComptesEvenementsLikedCompteId = 2, EvenementsLikedEvenementId = 3 },
+                    new { ComptesEvenementsLikedCompteId = 2, EvenementsLikedEvenementId = 1 },
+                    new { ComptesEvenementsLikedCompteId = 3, EvenementsLikedEvenementId = 2 },
+                    new { ComptesEvenementsLikedCompteId = 3, EvenementsLikedEvenementId = 1 },
+                    new { ComptesEvenementsLikedCompteId = 3, EvenementsLikedEvenementId = 3 }
+                ))
+                ;
+
             builder.Entity<Evenement>()
                 .HasOne(e => e.Compte)
                 .WithMany(c => c.EvenementsCrees)
@@ -25,7 +217,20 @@ namespace Colomb.Data
                 .OnDelete(DeleteBehavior.SetNull);
             builder.Entity<Compte>()
                .HasMany(c => c.ReviewsLiked)
-               .WithMany(e => e.ComptesReviewsLiked);
+               .WithMany(e => e.ComptesReviewsLiked)
+               .UsingEntity(j => j.HasData(
+                   new { ComptesReviewsLikedCompteId = 1, ReviewsLikedReviewId = 5 },
+                   new { ComptesReviewsLikedCompteId = 2, ReviewsLikedReviewId = 1 },
+                   new { ComptesReviewsLikedCompteId = 2, ReviewsLikedReviewId = 2 },
+                   new { ComptesReviewsLikedCompteId = 2, ReviewsLikedReviewId = 3 },
+                   new { ComptesReviewsLikedCompteId = 2, ReviewsLikedReviewId = 4 },
+                   new { ComptesReviewsLikedCompteId = 3, ReviewsLikedReviewId = 1 },
+                   new { ComptesReviewsLikedCompteId = 3, ReviewsLikedReviewId = 2 },
+                   new { ComptesReviewsLikedCompteId = 3, ReviewsLikedReviewId = 3 },
+                   new { ComptesReviewsLikedCompteId = 3, ReviewsLikedReviewId = 4 },
+                   new { ComptesReviewsLikedCompteId = 3, ReviewsLikedReviewId = 5 }
+                ))
+               ;
             builder.Entity<Review>()
                 .HasOne(r => r.Compte)
                 .WithMany(c => c.Reviews)
@@ -37,12 +242,10 @@ namespace Colomb.Data
                 .IsRequired(false)
                 .OnDelete(DeleteBehavior.SetNull);
 
+            
+
         }
 
-        public DbSet<Review> Reviews { get; set; } // Reviews = the name that the db will use
-        public DbSet<Evenement> Evenements { get; set; }
-        public DbSet<Compte> Comptes { get; set; }
-        /*public DbSet<CompteLikerEvenement> CompteLikerEvenements { get; set; }*/
 
     }
 }
---
+++ Migrations/20220409151751_SeedData.cs
@@ -0,0 +1,221 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Colomb.Migrations
+{
+    public partial class SeedData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Comptes",
+                columns: new[] { "CompteId", "Adresse", "DOB", "Description", "Email", "Login", "Nom", "NumeroSiret", "Password", "Photo", "Prenom", "Role", "VisibiliteReviews", "estValide" },
+                values: new object[] { 1, "40 rue de Compte", new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), "Description", "bogdan@gmail.com", "bogdan", "Radoi", null, "bogdan", "chemin/photo_compte_profil", "Bogdan", "ROLE_USER", true, true });
+
+            migrationBuilder.InsertData(
+                table: "Comptes",
+                columns: new[] { "CompteId", "Adresse", "DOB", "Description", "Email", "Login", "Nom", "NumeroSiret", "Password", "Photo", "Prenom", "Role", "VisibiliteReviews", "estValide" },
+                values: new object[] { 2, "40 rue de Paris", new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), "Description", "wally@gmail.com", "wally", "Cisse", null, "wally", "dossier/photo", "Wally", "ROLE_USER", true, true });
+
+            migrationBuilder.InsertData(
+                table: "Comptes",
+                columns: new[] { "CompteId", "Adresse", "DOB", "Description", "Email", "Login", "Nom", "NumeroSiret", "Password", "Photo", "Prenom", "Role", "VisibiliteReviews", "estValide" },
+                values: new object[] { 3, "40 rue de Paris", new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), "Description", "daria@gmail.com", "daria", "Rychkova", null, "Daria", "dossier/photo", "daria", "ROLE_USER", true, true });
+
+            migrationBuilder.InsertData(
+                table: "Evenements",
+                columns: new[] { "EvenementId", "Adresse", "Categorie", "CompteId", "Date", "Description", "EstSignale", "EstSuspendu", "HeureFermeture", "HeureOuverture", "Latitude", "Longitude", "Nom", "NombreLikes", "NombrePersMax", "NombreVues", "Photo", "Prix" },
+                values: new object[] { 1, "40 rue de Evenement", "musee", 1, new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), "Description evenement", false, false, new DateTime(2022, 12, 31, 18, 0, 0, 0, DateTimeKind.Unspecified), new DateTime(2022, 12, 31, 9, 0, 0, 0, DateTimeKind.Unspecified), 22.219999999999999, 40.219999999999999, "Evenement Comedie", 2, 50, 40, "chemin/photo_evenement", 40f });
+
+            migrationBuilder.InsertData(
+                table: "Evenements",
+                columns: new[] { "EvenementId", "Adresse", "Categorie", "CompteId", "Date", "Description", "EstSignale", "EstSuspendu", "HeureFermeture", "HeureOuverture", "Latitude", "Longitude", "Nom", "NombreLikes", "NombrePersMax", "NombreVues", "Photo", "Prix" },
+                values: new object[] { 3, "40 rue de Evenement", "spectacle", 1, new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), "Description evenement", false, false, new DateTime(2022, 12, 31, 17, 0, 0, 0, DateTimeKind.Unspecified), new DateTime(2022, 12, 31, 10, 0, 0, 0, DateTimeKind.Unspecified), 122.22, 140.22, "Evenement Parc", 112, 250, 140, "chemin/photo_evenement", 146f });
+
+            migrationBuilder.InsertData(
+                table: "Evenements",
+                columns: new[] { "EvenementId", "Adresse", "Categorie", "CompteId", "Date", "Description", "EstSignale", "EstSuspendu", "HeureFermeture", "HeureOuverture", "Latitude", "Longitude", "Nom", "NombreLikes", "NombrePersMax", "NombreVues", "Photo", "Prix" },
+                values: new object[] { 2, "40 rue de Evenement", "theatre", 2, new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), "Description evenement", false, false, new DateTime(2022, 12, 31, 19, 0, 0, 0, DateTimeKind.Unspecified), new DateTime(2022, 12, 31, 9, 0, 0, 0, DateTimeKind.Unspecified), 11.220000000000001, 55.219999999999999, "Evenement dehors", 440, 20, 140, "chemin/photo_evenement", 300f });
+
+            migrationBuilder.InsertData(
+                table: "CompteEvenement",
+                columns: new[] { "ComptesEvenementsLikedCompteId", "EvenementsLikedEvenementId" },
+                values: new object[,]
+                {
+                    { 2, 1 },
+                    { 3, 1 },
+                    { 1, 3 },
+                    { 2, 3 },
+                    { 3, 3 },
+                    { 3, 2 }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Reviews",
+                columns: new[] { "ReviewId", "CompteId", "Contenu", "Date", "EstSignale", "EvenementId", "NombreEtoiles", "NombreLikes" },
+                values: new object[,]
+                {
+                    { 3, 1, "Bref, je reviendrai pas", new DateTime(2022, 2, 15, 17, 0, 0, 0, DateTimeKind.Unspecified), false, 1, 1, 0 },
+                    { 1, 2, "Sympa mais pas plus, je veux veux un remboursement", new DateTime(2022, 12, 31, 17, 0, 0, 0, DateTimeKind.Unspecified), true, 3, 2, 2 },
+                    { 4, 2, "Ingenieux et spectaculaire", new DateTime(2022, 5, 17, 17, 0, 0, 0, DateTimeKind.Unspecified), false, 3, 3, 11 },
+                    { 5, 1, "Moyen mais pas cher, je ne recommande pas", new DateTime(2022, 9, 19, 17, 0, 0, 0, DateTimeKind.Unspecified), false, 3, 2, 22 },
+                    { 2, 3, "Tres bien", new DateTime(2022, 3, 14, 17, 0, 0, 0, DateTimeKind.Unspecified), false, 2, 5, 1 }
+                });
+
+            migrationBuilder.InsertData(
+                table: "CompteReview",
+                columns: new[] { "ComptesReviewsLikedCompteId", "ReviewsLikedReviewId" },
+                values: new object[,]
+                {
+                    { 2, 3 },
+                    { 3, 3 },
+                    { 2, 1 },
+                    { 3, 1 },
+                    { 2, 4 },
+                    { 3, 4 },
+                    { 1, 5 },
+                    { 3, 5 },
+                    { 2, 2 },
+                    { 3, 2 }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "CompteEvenement",
+                keyColumns: new[] { "ComptesEvenementsLikedCompteId", "EvenementsLikedEvenementId" },
+                keyValues: new object[] { 1, 3 });
+
+            migrationBuilder.DeleteData(
+                table: "CompteEvenement",
+                keyColumns: new[] { "ComptesEvenementsLikedCompteId", "EvenementsLikedEvenementId" },
+                keyValues: new object[] { 2, 1 });
+
+            migrationBuilder.DeleteData(
+                table: "CompteEvenement",
+                keyColumns: new[] { "ComptesEvenementsLikedCompteId", "EvenementsLikedEvenementId" },
+                keyValues: new object[] { 2, 3 });
+
+            migrationBuilder.DeleteData(
+                table: "CompteEvenement",
+                keyColumns: new[] { "ComptesEvenementsLikedCompteId", "EvenementsLikedEvenementId" },
+                keyValues: new object[] { 3, 1 });
+
+            migrationBuilder.DeleteData(
+                table: "CompteEvenement",
+                keyColumns: new[] { "ComptesEvenementsLikedCompteId", "EvenementsLikedEvenementId" },
+                keyValues: new object[] { 3, 2 });
+
+            migrationBuilder.DeleteData(
+                table: "CompteEvenement",
+                keyColumns: new[] { "ComptesEvenementsLikedCompteId", "EvenementsLikedEvenementId" },
+                keyValues: new object[] { 3, 3 });
+
+            migrationBuilder.DeleteData(
+                table: "CompteReview",
+                keyColumns: new[] { "ComptesReviewsLikedCompteId", "ReviewsLikedReviewId" },
+                keyValues: new object[] { 1, 5 });
+
+            migrationBuilder.DeleteData(
+                table: "CompteReview",
+                keyColumns: new[] { "ComptesReviewsLikedCompteId", "ReviewsLikedReviewId" },
+                keyValues: new object[] { 2, 1 });
+
+            migrationBuilder.DeleteData(
+                table: "CompteReview",
+                keyColumns: new[] { "ComptesReviewsLikedCompteId", "ReviewsLikedReviewId" },
+                keyValues: new object[] { 2, 2 });
+
+            migrationBuilder.DeleteData(
+                table: "CompteReview",
+                keyColumns: new[] { "ComptesReviewsLikedCompteId", "ReviewsLikedReviewId" },
+                keyValues: new object[] { 2, 3 });
+
+            migrationBuilder.DeleteData(
+                table: "CompteReview",
+                keyColumns: new[] { "ComptesReviewsLikedCompteId", "ReviewsLikedReviewId" },
+                keyValues: new object[] { 2, 4 });
+
+            migrationBuilder.DeleteData(
+                table: "CompteReview",
+                keyColumns: new[] { "ComptesReviewsLikedCompteId", "ReviewsLikedReviewId" },
+                keyValues: new object[] { 3, 1 });
+
+            migrationBuilder.DeleteData(
+                table: "CompteReview",
+                keyColumns: new[] { "ComptesReviewsLikedCompteId", "ReviewsLikedReviewId" },
+                keyValues: new object[] { 3, 2 });
+
+            migrationBuilder.DeleteData(
+                table: "CompteReview",
+                keyColumns: new[] { "ComptesReviewsLikedCompteId", "ReviewsLikedReviewId" },
+                keyValues: new object[] { 3, 3 });
+
+            migrationBuilder.DeleteData(
+                table: "CompteReview",
+                keyColumns: new[] { "ComptesReviewsLikedCompteId", "ReviewsLikedReviewId" },
+                keyValues: new object[] { 3, 4 });
+
+            migrationBuilder.DeleteData(
+                table: "CompteReview",
+                keyColumns: new[] { "ComptesReviewsLikedCompteId", "ReviewsLikedReviewId" },
+                keyValues: new object[] { 3, 5 });
+
+            migrationBuilder.DeleteData(
+                table: "Reviews",
+                keyColumn: "ReviewId",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Reviews",
+                keyColumn: "ReviewId",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Reviews",
+                keyColumn: "ReviewId",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Reviews",
+                keyColumn: "ReviewId",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Reviews",
+                keyColumn: "ReviewId",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "Comptes",
+                keyColumn: "CompteId",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Evenements",
+                keyColumn: "EvenementId",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Evenements",
+                keyColumn: "EvenementId",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Evenements",
+                keyColumn: "EvenementId",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Comptes",
+                keyColumn: "CompteId",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Comptes",
+                keyColumn: "CompteId",
+                keyValue: 2);
+        }
+    }
+}
